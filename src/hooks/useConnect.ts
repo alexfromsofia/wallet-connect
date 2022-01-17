4f0311b85cffa7297a5d711adb0afdb69a5e4ef2
Alexander Georgiev <aleksandar_georgiev@starsgroup.com>
2022-01-17 11:34:25 +0200

    Fetch web3 data on login and on refresh

--- src/hooks/useConnect.ts
+++ src/hooks/useConnect.ts
@@ -9,57 +9,71 @@ const tokens = allTokens.tokens as Token[]
 
 const useConnect = () => {
   const { setState, state } = useGlobalContext()
-
+  const { isMetaMaskLinked, address } = state
   useEffect(() => {
     const tokenIds: string[] = []
     const tokensById: TokenById = {}
+
+    if (isMetaMaskLinked && address) return
+
     async function getWalletData() {
       const chainId: any = await ethereum.request({ method: "eth_chainId" })
       const [address]: any = await ethereum.request({ method: "eth_accounts" })
+      const walletLinked = typeof address !== "undefined"
 
-      tokens.forEach(async (token) => {
-        const { address, symbol } = token
-        const contract = new web3Instance.eth.Contract(abi as any, address, {
-          from: web3Instance.eth.defaultAccount as string,
+      if (walletLinked) {
+        tokens.forEach(async (token) => {
+          const { address, symbol } = token
+          const contract = new web3Instance.eth.Contract(abi as any, address, {
+            from: web3Instance.eth.defaultAccount as string,
+          })
+
+          const balance = await contract.methods.balanceOf(address).call()
+
+          tokenIds.push(symbol)
+          tokensById[symbol] = { ...token, balance }
         })
-
-        const balance = await contract.methods.balanceOf(address).call()
-
-        tokenIds.push(symbol)
-        tokensById[symbol] = { ...token, balance }
-      })
-
-      const isMetaMaskLinked = typeof address !== "undefined"
-
-      if (isMetaMaskLinked) {
         const balance = await web3Instance.eth.getBalance(address)
 
         tokenIds.push(ETH.symbol)
-        tokensById[ETH.symbol] = { ...ETH, balance: Number(balance) }
+        tokensById[ETH.symbol] = {
+          ...ETH,
+          balance: Number(web3Instance.utils.fromWei(balance)),
+        }
 
         setState({
           ...state,
           isConnected: true,
-          isMetaMaskLinked,
+          isMetaMaskLinked: walletLinked,
           chainId,
           address,
           tokenIds,
           tokensById,
         })
+      } else {
+        setState({
+          ...state,
+          isConnected: true,
+        })
       }
     }
 
+    getWalletData()
+  }, [isMetaMaskLinked, address])
+
+  useEffect(() => {
     ethereum.on("chainChanged", () => {
       window.location.reload()
     })
 
     ethereum.on("accountsChanged", async (accounts: any) => {
       if (!accounts || !accounts.length) {
-        setState(initialState)
+        setState({
+          ...initialState,
+          isConnected: true,
+        })
       }
     })
-
-    getWalletData()
   }, [])
 }
 
